4260ac678e35ff7cdaa30d02def2f3c6a6d8dc62
Sultan1901 <sultan.aqeel.elq.js@tuwaiq.edu.sa>
2021-10-14 11:57:51 +0300

    add first task of javascript

---
+++ myvariable.js
@@ -0,0 +1,10 @@
+let language = 'javascript';
+let score = 10;
+console.log( language , score);
+let width = 10;
+let length = 10;
+let memberarea = width * length
+console.log(memberarea)
+let num1 = 6
+let num2 = 6
+console.log(num1 % num2)
