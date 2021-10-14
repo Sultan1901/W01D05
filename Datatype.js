ac64e10b65fc2fffd2130ce3eb15c02d16515403
Sultan1901 <sultan.aqeel.elq.js@tuwaiq.edu.sa>
2021-10-14 12:23:12 +0300

    second task

---
+++ Datatype.js
@@ -0,0 +1,12 @@
+let num = 123;
+let string = "abc";
+let array = ["Sultan", "Sulaiman", "Ahmad"];
+let obj = { name: "ss", num2: 123 };
+console.log(typeof num);
+console.log(typeof string);
+console.log(typeof array);
+console.log(typeof obj);
+let arr1 = ["Sultan", 123]; // I Expect it to be typeof object
+console.log(typeof arr1);
+let nul = null;
+let undif;
--- myvariable.js
+++ myvariable.js
@@ -8,3 +8,5 @@ console.log(memberarea)
 let num1 = 6
 let num2 = 6
 console.log(num1 % num2)
+
+let arr = []
