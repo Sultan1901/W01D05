008a7fc66d99a4028aa919b29cceb6ee026dde86
Sultan1901 <sultan.aqeel.elq.js@tuwaiq.edu.sa>
2021-10-16 17:21:32 +0300

    update with no help or references

--- controlstructures.js
+++ controlstructures.js
@@ -1,57 +1,67 @@
+// first task
 let num1 = 1;
-let num2 = 2;
-let num3 = 3;
-
-if (num2 > num1) {
+let num2 = 10;
+let num3 = 15;
+if (num1 < num2) {
   console.log(num2);
-} else if (num1 > num2) {
-  console.log(num1);
-}
-
-//if (num1 % 2 == 1){
-
-//   console.log('its odd');
-//} else{console.log('its not odd');}
-//let i = unm1
-//for ( let i = 0; i > 0 ; i--)
-//{ i+= console.log(i);
-//} ( **** i try for this code but cant run seccessfuly )
-
-let ammount = 9;
-if (ammount <= 0) {
-  console.log("please inter a postitive number");
-} else if (ammount > 0) {
-  console.log(ammount);
 } else {
-  console.log(" please inter numbers");
+  console.log("numbers are equal");
 }
-
-let username = "name";
-let password = "pass";
-if (username.length >= 6 && password.length >= 8) {
-  console.log("login successful");
+// second task
+if (num1 % num2 == 1) {
+  console.log("its odd");
 } else {
-  console.log("login failed");
+  console.log("its not odd");
 }
+//third task
+if ((num1 > num2 && num3 > num1, num2)) {
+  console.log(num3);
+} else {
+  console.log(num1, num2, num3);
+}
+//fourth task
+let amount = "";
+if (amount >= 0) {
+  console.log("its ammount");
+} else if (amount < 0) {
+  console.log("its not amount");
+} else console.log("please inter amount");
 
+//fifth task
+let username = "";
+let password = "";
+if ((username.length >= 6, password.length >= 8)) {
+  console.log("login successfully");
+} else console.log("login failed");
+//sixth task
 let i = 0;
-while (i <= 20) {
+for (i = 0; i <= 20; i++) {
   console.log(i);
-  i++;
 }
-let startRange = 6;
-let endRange = 18;
-for (startRange; startRange <= endRange; startRange++) {
-  console.log(startRange);
+//seventh task
+let n = 20;
+for (n = 20; n >= 0; n--) {
+  console.log(n);
 }
-
-for (let i = 1; i <= 5; i++) {
-  console.log("*".repeat(i));
+// eathth task
+let n0 = 1;
+for (n0 = 1; n0 < 20; n0++) {
+  console.log(n0);
+}
+//nineth task
+let startrange = 2;
+let endrange = 12;
+for (startrange = 2; startrange < endrange; startrange++) {
+  console.log(startrange);
+}
+//tenth task
+let str = 1;
+for (str = 1; str < 6; str++) {
+  console.log("*".repeat(str));
 }
 
 // ===> optional
 
- for (let arr = [5 , 10] ; arr <= 100 ; arr++) {
-  console.log(arr * 2)
-
+for (let arr = [5, 10]; arr <= 100; arr++) {
+  console.log(arr * 2);
 }
